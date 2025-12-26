a86817a2a697c1f55e459ef779791079df28432f
Vladyslav Zaiets <rutova2@gmail.com>
2025-12-26 06:49:56 +0200

    refactor: cleanup in ActorSystemBenchmarks

--- benchmarks/DotNetActorFramework.Benchmarks/ActorSystemBenchmarks.cs
+++ benchmarks/DotNetActorFramework.Benchmarks/ActorSystemBenchmarks.cs
@@ -3,12 +3,25 @@ using DotNetActorFramework.Models;
 
 namespace DotNetActorFramework.Benchmarks;
 
+/// <summary>
+/// Benchmark class for ActorSystem performance.
+/// </summary>
 [MemoryDiagnoser]
 public class ActorSystemBenchmarks
 {
+    /// <summary>
+    /// The ActorSystem instance used for benchmarking.
+    /// </summary>
     private ActorSystem? _system;
+
+    /// <summary>
+    /// The ActorPath used for benchmarking.
+    /// </summary>
     private ActorPath _path;
 
+    /// <summary>
+    /// Sets up the ActorSystem and ActorPath for benchmarking.
+    /// </summary>
     [GlobalSetup]
     public void Setup()
     {
@@ -16,18 +29,29 @@ public class ActorSystemBenchmarks
         _path = new ActorPath("/test-actor");
     }
 
+    /// <summary>
+    /// Creates a new ActorRef asynchronously and measures the time it takes.
+    /// </summary>
+    /// <returns>The created ActorRef.</returns>
     [Benchmark]
     public async Task<ActorRef> CreateActorAsync()
     {
         return await _system!.CreateActorAsync(new ActorPath("/new-actor-" + Guid.NewGuid()));
     }
 
+    /// <summary>
+    /// Gets the ActorRef for the specified ActorPath and measures the time it takes.
+    /// </summary>
     [Benchmark]
     public void GetActorRef()
     {
         _system!.GetActorRef(_path);
     }
 
+    /// <summary>
+    /// Gets the SystemHealthSummary for the ActorSystem and measures the time it takes.
+    /// </summary>
+    /// <returns>The SystemHealthSummary.</returns>
     [Benchmark]
     public SystemHealthSummary GetHealthSummary()
     {
