70313a551724f8289a5af85898748bf3316b048a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 10:31:59 +0300

    refactor(models): tighten guard clauses in ActorExtensions

--- src/DotNetActorFramework/Models/ActorExtensions.cs
+++ src/DotNetActorFramework/Models/ActorExtensions.cs
@@ -9,8 +9,8 @@ public static class ActorExtensions
     /// Checks if the actor is terminated.
     /// </summary>
     /// <param name="actor">The actor to check.</param>
-    /// <returns><c>true</c> if the actor is terminated; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="actor"/> is <c>null</c>.</exception>
+    /// <returns><see langword="true"/> if the actor is terminated; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="actor"/> is <see langword="null"/>.</exception>
     public static bool IsTerminated(this Actor actor)
     {
         ArgumentNullException.ThrowIfNull(actor);
@@ -21,8 +21,8 @@ public static class ActorExtensions
     /// Checks if the actor is active.
     /// </summary>
     /// <param name="actor">The actor to check.</param>
-    /// <returns><c>true</c> if the actor is active; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="actor"/> is <c>null</c>.</exception>
+    /// <returns><see langword="true"/> if the actor is active; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="actor"/> is <see langword="null"/>.</exception>
     public static bool IsActive(this Actor actor)
     {
         ArgumentNullException.ThrowIfNull(actor);
@@ -34,7 +34,7 @@ public static class ActorExtensions
     /// </summary>
     /// <param name="actor">The actor to get metrics for.</param>
     /// <returns>A summary of the actor's metrics.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="actor"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="actor"/> is <see langword="null"/>.</exception>
     public static ActorMetricsSummary GetMetricsSummary(this Actor actor)
     {
         ArgumentNullException.ThrowIfNull(actor);
