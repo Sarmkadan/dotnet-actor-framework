efbb6caecd91ce0251e2a13d9a5016d6cacee709
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 07:40:20 +0300

    chore(dotnet-actor-framework.tests): update ActorTests

---
+++ tests/dotnet-actor-framework.Tests/ActorTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using DotNetActorFramework.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNetActorFramework.Tests
+{
+    public class ActorTests
+    {
+        // Write unit tests for Actor
+    }
+}
