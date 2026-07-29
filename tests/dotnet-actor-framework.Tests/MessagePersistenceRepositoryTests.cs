e7558b098da6ed4c304c6ae908188199b08b88d2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 11:51:37 +0300

    update MessagePersistenceRepositoryTests

---
+++ tests/dotnet-actor-framework.Tests/MessagePersistenceRepositoryTests.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace dotnet_actor_framework.Tests
+{
+    public class MessagePersistenceRepositoryTests
+    {
+        // TODO: Write tests
+    }
+}
