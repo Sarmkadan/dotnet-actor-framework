7a0d247d0db83be3a2503c88f53e40ac40c02174
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 12:07:58 +0300

    tweak ActorSystemBuilderJsonExtensionsTests

---
+++ tests/dotnet-actor-framework.Tests/ActorSystemBuilderJsonExtensionsTests.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DotNetActorFramework.Tests
+{
+    public class ActorSystemBuilderJsonExtensionsTests
+    {
+        // TODO: Write unit tests for ActorSystemBuilderJsonExtensions
+    }
+}
