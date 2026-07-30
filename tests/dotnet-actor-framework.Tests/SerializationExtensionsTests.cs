555b578bdf196fb70b4d481ec231b81b946c391a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    update SerializationExtensionsTests

---
+++ tests/dotnet-actor-framework.Tests/SerializationExtensionsTests.cs
@@ -0,0 +1,63 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using DotNetActorFramework.Utilities;
+
+namespace DotNetActorFramework.Tests
+{
+    public class SerializationExtensionsTests
+    {
+        [Fact]
+        public async Task TestToJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToJsonPrettyAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToJsonBytesAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJsonBytesAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestTryFromJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestIsValidJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestDeepCopyAsync()
+        {
+            // Test implementation here
+        }
+    }
+}
