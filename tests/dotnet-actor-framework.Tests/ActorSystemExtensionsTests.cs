4983ec0e1412e8c7c29e84167aba5040cfee0cb2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 21:30:00 +0300

    Get ActorSystemExtensionsTests compilnig again

---
+++ tests/dotnet-actor-framework.Tests/ActorSystemExtensionsTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+using DotNetActorFramework.Models;
+
+namespace DotNetActorFramework.Tests
+{
+    public class ActorSystemExtensionsTests
+    {
+        [Fact]
+        public void IsShutdown_Happy_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsActive_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetUptime_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsShutdown_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsActive_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetUptime_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
