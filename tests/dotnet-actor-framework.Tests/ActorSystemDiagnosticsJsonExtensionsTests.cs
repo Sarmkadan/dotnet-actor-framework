f1f9ffe3f0cf7d853d0ba08a4293e7ae497971e7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 08:07:55 +0300

    ActorSystemDiagnosticsJsonExtensionsTests changes
    will circle back to tests later.

---
+++ tests/dotnet-actor-framework.Tests/ActorSystemDiagnosticsJsonExtensionsTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNetActorFramework;
+using DotNetActorFramework.Diagnostics;
+using Newtonsoft.Json;
+
+namespace DotNetActorFramework.Tests
+{
+    public class ActorSystemDiagnosticsJsonExtensionsTests
+    {
+        public void TestToJson() { Assert.Fail("Not implemented"); }
+    }
+}
