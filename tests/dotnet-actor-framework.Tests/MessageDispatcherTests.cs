bfa825283d7d20785ee520ab34e04fdf8f5d57b2
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-02 01:48:23 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-actor-framework.Tests/MessageDispatcherTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using DotNetActorFramework.Services;
+using DotNetActorFramework.Exceptions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetActorFramework.Tests
+{
+    public class MessageDispatcherTests
+    {
+        // Test methods go here
+    }
+}
