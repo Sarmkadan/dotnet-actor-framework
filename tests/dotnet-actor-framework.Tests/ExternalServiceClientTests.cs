4a805013d325cb2288fbc4b088bf2c01f45871a0
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-05 05:29:56 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-actor-framework.Tests/ExternalServiceClientTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using System.Threading.Tasks;
+using DotNetActorFramework.Integration;
+using DotNetActorFramework.Tests;
+
+namespace DotNetActorFramework.Tests
+{
+    public class ExternalServiceClientTests
+    {
+        // Test methods go here
+    }
+}
