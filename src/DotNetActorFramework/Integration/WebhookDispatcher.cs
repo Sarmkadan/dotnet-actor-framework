549e30ad23d4522ae65bf9fd76f0a2af3d62e001
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-16 23:36:24 +0000

    chore: maintenance updates

--- src/DotNetActorFramework/Integration/WebhookDispatcher.cs
+++ src/DotNetActorFramework/Integration/WebhookDispatcher.cs
@@ -4,7 +4,6 @@
 // =============================================================================
 
 using System.Text;
-using System.Text.Json;
 using DotNetActorFramework.Events;
 using DotNetActorFramework.Utilities;
 
